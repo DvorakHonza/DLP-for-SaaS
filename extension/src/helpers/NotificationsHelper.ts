6226cf12a96d145bfefd17294b5786978f9d1187
DvorakHonza <jan.dvorak@safetica.com>
2022-04-18 23:25:47 +0200

    Minor fixes and localization added

--- extension/src/helpers/NotificationsHelper.ts
+++ extension/src/helpers/NotificationsHelper.ts
@@ -1,40 +1,50 @@
 import { OperationType } from "../Enums/OperationType";
 import { PolicyMode } from "../Enums/PolicyMode";
 
-export class NotificationHelper {
+export class NotificationsHelper {
 
     private static operationNotifications: {[operation in OperationType]?: any} = {
         [OperationType.Upload]: {
             message: chrome.i18n.getMessage('uploadNotificationMessage'),
-            iconUrl: './images/file_upload.svg'
+            iconUrl: './images/icons/file_upload.svg'
         },
         [OperationType.Print]: {
             message: chrome.i18n.getMessage('printNotificationMessage'),
-            iconUrl: './images/print.svg'
+            iconUrl: './images/icons/print.svg'
         }
     }
 
     private static modeNotifications: {[mode in PolicyMode]?: any} = {
         [PolicyMode.Block]: {
-            title: '',
+            title: 'Operation was blocked in accordance with security policy.',
             type: 'basic',
         },
         [PolicyMode.Notify]: {
             title: chrome.i18n.getMessage('notifyNotificationTitle'),
             type: 'basic',
             buttons: [
-                { title: 'Proceed' },
-                { title: 'Cancel' }
+                { title: chrome.i18n.getMessage('proceedButtonTitle') },
+                { title: chrome.i18n.getMessage('cancelButtonTitle') }
             ],
             requireInteraction: true,
         }
     }
 
     public static showNotification(mode: PolicyMode, operation: OperationType) {
+        console.log('Showing notification');
         let notificationOptions = {
             ...this.operationNotifications[operation],
             ...this.modeNotifications[mode]
         }
-        chrome.notifications.create('', notificationOptions);
+        chrome.notifications.create('a', notificationOptions);
+    }
+
+    public static buttonClickedHandler(notificationId: string, buttonIndex: number) {
+        if (buttonIndex == 0) {
+            console.log('Proceed button clicked');
+        }
+        else if (buttonIndex == 1) {
+            console.log('Cancel button clicked');
+        }
     }
 }
